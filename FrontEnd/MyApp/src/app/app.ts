0d6da02487bf7d63809b716a1152a0a1a3a0772e
Louey Foudhaili <Loueyfoudhaili1@gmail.com>
2026-02-14 22:16:02 +0100

    Add employees UI and projects/teams modules

--- FrontEnd/MyApp/src/app/app.ts
+++ FrontEnd/MyApp/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet, RouterLinkActive, Router } from '@angular/router';
+import { RouterLink } from '@angular/router'; 
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive , CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('MyApp');
+
+  constructor(private router : Router) {}
+
+  isLoginPage(): boolean {
+    return this.router.url === '/login';
+  }
+   
 }
